4634a1877381160c6629d1f5ee7fa568b220ada7
FengZhang <zfcode@qq.com>
2016-11-29 21:29:08 +0800

    2016年11月29日21:28:57

--- UI/src/assets/componets/menu.vue
+++ UI/src/assets/componets/menu.vue
@@ -1,4 +1,5 @@
 <template>
+<<<<<<< Updated upstream
   <div>
     <i class='fa fa-align-justify icon-large' id='toggle'></i>
     <ul id='dock'>
@@ -38,6 +39,46 @@
       <img :src="img_per" />
     </div>
   </div>
+=======
+<div>
+      <i class='fa fa-align-justify icon-large' id='toggle'></i>
+      <ul id='dock'>
+        <li class='active launcher'>
+          <i class="fa fa-desktop"></i>
+          <router-link :to="{ name: 'home' }">首页</router-link>
+        </li>
+        <li class='launcher' >
+          <i class="fa  fa-cloud"></i>
+          <a href="#">云产品库</a>
+        </li>
+        <li class='launcher'>
+          <i class="fa  fa-shopping-cart"></i>
+          <a href="#">导购系统</a>
+        </li>
+        <li class='launcher'>
+          <i class="fa fa-retweet"></i>
+          <a href="#">消息转换</a>
+        </li>
+        <li class='launcher'>
+          <i class="fa fa-comments"></i>
+          <a href="#">群消息监控</a>
+        </li>
+        <li class='launcher'>
+          <i class="fa fa-cubes"></i>
+          <a href="#">实用工具</a>
+        </li>
+        <li class='launcher'>
+          <i class="fa fa-building-o"></i>
+          <a href="#">订单查询</a>
+        </li>
+
+
+      </ul>
+      <div id='beaker'>
+        <img :src="img_per" />
+      </div>
+      <div>
+>>>>>>> Stashed changes
 </template>
 <script>
 
@@ -48,5 +89,6 @@
           img_per:require('../images/person.png')
         }
     }
+    
   }
 </script>
--- UI/src/main.js
+++ UI/src/main.js
@@ -1,13 +1,22 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import ElementUI from 'element-ui'
+<<<<<<< Updated upstream
 import VueRouter from 'vue-router';
 import 'element-ui/lib/theme-default/index.css'
 import App from './App.vue'
+=======
+import App from './App.vue'
+//import Menu from './assets/componets/menu.vue'
+import Index from './assets/componets/Index.vue'
+import 'element-ui/lib/theme-default/index.css'
+>>>>>>> Stashed changes
 import './assets/stylesheets/application-a07755f5.css'
 import './assets/font-awesome/css/font-awesome.min.css'
 import Index from './assets/pages/index.vue';
 
 Vue.use(ElementUI)
+<<<<<<< Updated upstream
 Vue.use(VueRouter);
 const routes = [
     { path: '/', component: App,
@@ -30,7 +39,33 @@ const router = new VueRouter({
 const app = new Vue({
     router
 }).$mount('#app');
+=======
+//Vue.use(VueRouter)
+
+>>>>>>> Stashed changes
 
 
 
 
+// new Vue({
+//   el:"#app_c",
+//   render:h=>h(Index)
+// })
+
+
+
+const router = new VueRouter({
+  
+  routes: [
+    { path: '/', name: 'home', component: Index }
+  ]
+})
+
+// new Vue({
+//     render: h => h(App)
+// }).$mount('#app_c');
+// new Vue({
+//     router:router,
+//   el: '#app_c'
+// })
+const app = new Vue({ router:router }).$mount('#wrapper');  
