7e86792e954013135a189b3bf4bc2d8a9787ec47
FengZhang <zfcode@qq.com>
2016-11-29 21:36:22 +0800

    Revert "2016年11月29日21:28:57"
    This reverts commit 4634a1877381160c6629d1f5ee7fa568b220ada7.

--- UI/src/assets/componets/menu.vue
+++ UI/src/assets/componets/menu.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< Updated upstream
   <div>
     <i class='fa fa-align-justify icon-large' id='toggle'></i>
     <ul id='dock'>
@@ -39,46 +38,6 @@
       <img :src="img_per" />
     </div>
   </div>
-=======
-<div>
-      <i class='fa fa-align-justify icon-large' id='toggle'></i>
-      <ul id='dock'>
-        <li class='active launcher'>
-          <i class="fa fa-desktop"></i>
-          <router-link :to="{ name: 'home' }">首页</router-link>
-        </li>
-        <li class='launcher' >
-          <i class="fa  fa-cloud"></i>
-          <a href="#">云产品库</a>
-        </li>
-        <li class='launcher'>
-          <i class="fa  fa-shopping-cart"></i>
-          <a href="#">导购系统</a>
-        </li>
-        <li class='launcher'>
-          <i class="fa fa-retweet"></i>
-          <a href="#">消息转换</a>
-        </li>
-        <li class='launcher'>
-          <i class="fa fa-comments"></i>
-          <a href="#">群消息监控</a>
-        </li>
-        <li class='launcher'>
-          <i class="fa fa-cubes"></i>
-          <a href="#">实用工具</a>
-        </li>
-        <li class='launcher'>
-          <i class="fa fa-building-o"></i>
-          <a href="#">订单查询</a>
-        </li>
-
-
-      </ul>
-      <div id='beaker'>
-        <img :src="img_per" />
-      </div>
-      <div>
->>>>>>> Stashed changes
 </template>
 <script>
 
@@ -89,6 +48,5 @@
           img_per:require('../images/person.png')
         }
     }
-    
   }
 </script>
